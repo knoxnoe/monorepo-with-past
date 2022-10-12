ff10adf69d524792e7faa4a6a8f8fe56984a66db
noe <gnoe@foxmail.com>
2022-10-12 21:41:12 +0800

    clock in commit

---
+++ apps/with-anu/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { ABtn, ARadio } from 'anu-vue'
+import { ref } from 'vue'
+
+const favoriteFruit = ref(null)
+</script>
+
+<template>
+  <div>
+    <ABtn>Button</ABtn>
+    <ARadio
+      v-model="favoriteFruit"
+      name="fruit"
+      value="banana"
+      label="Banana"
+    />
+  </div>
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ apps/with-anu/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// import './style.css'
+import App from './App.vue'
+import 'uno.css'
+import 'anu-vue/dist/style.css'
+
+createApp(App).mount('#app')
